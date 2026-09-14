e4a71fa6695d8b9374695473071d241531601b52
Florian Perréon <florian.perreon@gmail.com>
2026-09-14 21:34:21 +0200

    Mise a jour du portail (Claude) 14/09/2026 21:34

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    - La page et meta.js sont demandés en « no-cache » : le navigateur revalide toujours auprès
      du serveur (304 si rien n'a changé), au lieu de servir sa copie pendant 10 minutes.
      C'est ce qui empêchait de voir une mise en ligne récente. */
-const VERSION = 'kairos-b7e9c6bcb4fc28';
+const VERSION = 'kairos-bd37ce3e672dd5';
 const CORE = ['./', './index.html', './meta.js', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', e => {
